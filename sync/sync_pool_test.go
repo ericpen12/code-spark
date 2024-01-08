0a6027d4b75b6d37e1d33671beded1ea6feac492
彭巧玉 <pengqy@weipaitang.com>
2024-01-08 15:24:52 +0800

    新增 queue.go

--- sync/sync_pool_test.go
+++ sync/sync_pool_test.go
@@ -1,7 +1,6 @@
-package std
+package sync
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 )
@@ -25,46 +24,14 @@ func TestSyncPool(t *testing.T) {
 	//t.Log(a2.Name)
 }
 
-type queue struct {
-	headTail int64
-	data     []int
-}
-
-const queueBit = 4
-
 func TestQueue(t *testing.T) {
 	q := &queue{}
 	for i := 0; i < 5; i++ {
 		q.enQueue(1)
+		q.info("入队")
 	}
 	for i := 0; i < 5; i++ {
 		q.deQueue()
+		q.info("出队")
 	}
-}
-
-func (q *queue) enQueue(data int) {
-	q.data = append(q.data, data)
-	q.headTail += 1 << queueBit
-	q.info("入队")
-}
-
-func (q *queue) deQueue() {
-	q.data = q.data[1:]
-	head, tail := q.unpack()
-	head--
-	const mask = 1<<queueBit - 1
-	q.headTail = (head << queueBit) | (tail & mask)
-	q.info("出队")
-}
-
-func (q *queue) unpack() (int64, int64) {
-	const mask = 1<<queueBit - 1
-	head := (q.headTail >> queueBit) & mask
-	tail := q.headTail & mask
-	return head, tail
-}
-
-func (q *queue) info(opt string) {
-	head, tail := q.unpack()
-	fmt.Printf("操作： %s headTail: %8b, head: %8b, tail:%8b\n", opt, q.headTail, head, tail)
 }
